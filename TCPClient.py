9d3583060b6ceef443a5362d4b7641e9c1bdeb30
ikromshi <numanovikrom454@gmail.com>
2023-09-08 12:25:29 -0400

    py server implemented; small changes

--- TCPClient.py
+++ TCPClient.py
@@ -1,11 +1,11 @@
 from socket import *
 
 serverName = "localhost"
-serverPort = 6789
+serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 sentence = input("Input a lowercase sentence: ")
-clientSocket.send((sentence + "\n").encode())
+clientSocket.send((sentence).encode())
 modifiedSentence = clientSocket.recv(1024)
 print("From Server: ", modifiedSentence.decode())
 clientSocket.close()
---
+++ TCPServer.py
@@ -0,0 +1,14 @@
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(("", serverPort))
+serverSocket.listen(1)
+print("Server is ready to receive;")
+
+while True:
+    connectionSocket, address = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence.encode())
+    connectionSocket.close()
